93e3de5cbfda3085f3ea2b32144d07d11a1e88b1
yc-ng <mizzerische@yahoo.com>
2018-04-19 15:00:26 +0800

    minor changes, update tree image

--- 01_intro.R
+++ 01_intro.R
@@ -20,7 +20,6 @@ test <- shuffled[(round(n * frac) + 1):n, ]
 train_lab <- train$class
 test_lab <- test$class
 
-
 # alternatively, if all rows in mushrooms are unique (duplicated())
 # train <- sample_frac(mushrooms, size = 0.7)
 # test <- setdiff(mushrooms, train)
@@ -33,7 +32,7 @@ library(rpart.plot)
 set.seed(50)
 mush_tree1 <- rpart(class ~ ., data = train, method = "class")
 
-rpart.plot(mush_tree1, type = 4, extra = 101, under = TRUE)
+rpart.plot(mush_tree1, type = 4, extra = 101)
 
 #' interpreting the tree:
 #' oodor == cfmpsy ~ poisonous
@@ -41,6 +40,19 @@ rpart.plot(mush_tree1, type = 4, extra = 101, under = TRUE)
 #' odor == aln & spore.print.color == bhknouwy ~ edible
 
 mush_pred1 <- predict(mush_tree1, newdata = test, type = "class")
-(mush_confus1 <- table(mush_pred1, test_lab))
+(mush_confus1 <- table(mush_pred1, test$class))
 (mush_acc1 <- sum(diag(mush_confus1) / sum(mush_confus1)))
 
+# finding out which mushrooms were misclassified
+test %>%
+    add_column(mush_pred1) %>% 
+    filter(class == "p" & mush_pred1 == "e") %>% 
+    select(odor, spore.print.color, mush_pred1, class)
+#' all 16 misclassifications had no odor and white spore prints
+
+test %>%
+    add_column(mush_pred1) %>% 
+    filter(odor == "n" & spore.print.color == "w") %>% 
+    summarise(correct = sum(mush_pred1 == class), 
+              wrong = sum(mush_pred1 != class))
+#' 207 mushrooms had no odor, white spore prints; 191 correct 16 wrong  
